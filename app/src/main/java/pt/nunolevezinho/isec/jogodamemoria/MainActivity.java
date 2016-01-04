2e3173ab5676f1c71b9f3ecb8d3b749bba6e911f
Nuno <gamesflash94@gmail.com>
2016-01-04 17:07:43 +0000

    New Screens and Preferences

--- app/src/main/java/pt/nunolevezinho/isec/jogodamemoria/MainActivity.java
+++ app/src/main/java/pt/nunolevezinho/isec/jogodamemoria/MainActivity.java
@@ -1,5 +1,8 @@
 package pt.nunolevezinho.isec.jogodamemoria;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,8 +10,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String MyPREFERENCES = "MemoryGamePrefs";
+    Button singlePlayer;
+    SharedPreferences sharedpreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +25,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
+
+        //First Time Run ?
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        int first = sharedpreferences.getInt("firstRun", 0);
+        Toast.makeText(getApplicationContext(), Integer.toString(first), Toast.LENGTH_LONG).show();
+        if (first == 0) {
+            //TODO: Insert DialogFragment
+            String usernameTemp = "trololo";
+            SharedPreferences.Editor editor = getSharedPreferences(MyPREFERENCES, MODE_PRIVATE).edit();
+            editor.putInt("firstRun", 1);
+            editor.apply();
+        }
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -29,6 +51,19 @@ public class MainActivity extends AppCompatActivity {
         //TextView tv=(TextView)findViewById(R.id.button);
         //Typeface face= Typeface.createFromAsset(getAssets(), "fonts/SF Atarian System.ttf");
         //tv.setTypeface(face);
+        singlePlayer = (Button) findViewById(R.id.button);
+        singlePlayer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent(getApplicationContext(), SPLevelSelect.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void getUsernameDialog() {
+
     }
 
     @Override
---
+++ app/src/main/java/pt/nunolevezinho/isec/jogodamemoria/SPLevelSelect.java
@@ -0,0 +1,17 @@
+package pt.nunolevezinho.isec.jogodamemoria;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by nunol on 1/4/2016.
+ */
+public class SPLevelSelect extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.level_select);
+
+
+    }
+}
